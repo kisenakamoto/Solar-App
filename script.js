3478766aa22a6b6d048f1d31dacbd898be2dfed3
Gem Ryan <gembencito@gmail.com>
2023-03-10 22:09:15 +0800

    fix weather api location

--- script.js
+++ script.js
@@ -16,13 +16,28 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
+
+  fetchWeatherLat: function () {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?lat=14.197&lon=121.02&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     const { all } = data.clouds;
-    document.querySelector(".city").innerText = name + ", Cavite";
+    document.querySelector(".city").innerText = name ;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
@@ -53,4 +68,4 @@ document
     }
   });
 
-weather.fetchWeather("Silang");
+weather.fetchWeatherLat();
